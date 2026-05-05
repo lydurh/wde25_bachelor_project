5adbec0ed6551fa30882a0d6922dcd87ebe91d67
anton0998 <anton.heiden@hotmail.com>
2026-05-05 14:13:38 +0200

    Chore(code-rules): Code linting/formating

--- packages/config/eslint/base.mjs
+++ packages/config/eslint/base.mjs
@@ -3,8 +3,49 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      quotes: ["error", "single", { avoidEscape: true }],
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { arguments: false } },
+      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "all",
+          ignoreRestSiblings: false,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "ImportDefaultSpecifier",
+          message: "Use named imports instead of default imports.",
+        },
+        {
+          selector: "ExportDefaultDeclaration",
+          message: "Use named exports instead of default exports.",
+        },
+      ],
+    },
     ignores: ["dist/", "node_modules/"],
   },
 );
