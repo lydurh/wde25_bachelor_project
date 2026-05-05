3106658a664614209dd3a48b1dc81f5dfc26581c
Lydur Hafsteinsson <lydur.hafsteinsson@gmail.com>
2026-05-05 12:25:43 +0200

    Initial docker set up, added docker-compose and /admin/Dockerfile

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 
 const app = new Hono();
-app.get("/", (c) => c.json({ status: "ok" }));
+app.get("/", (c) => c.json({ status: "Hello world!" }));
 
 export default app;
