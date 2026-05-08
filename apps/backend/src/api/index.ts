373d891501a816ca442d710bd8000849a053ff52
anton0998 <anton.heiden@hotmail.com>
2026-05-08 13:48:07 +0200

    feat(api): Included sample endpoint for appointments(#140)

--- apps/backend/src/api/index.ts
+++ apps/backend/src/api/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import { health } from './health';
 import { usersRoutes } from './users/users.router';
+import { appointmentsRouter } from './appointments/appointments.router';
 
 const api = new Hono();
 
 api.route('/health', health);
 api.route('/users', usersRoutes);
+api.route('/appointments', appointmentsRouter); 
 
 export { api };
