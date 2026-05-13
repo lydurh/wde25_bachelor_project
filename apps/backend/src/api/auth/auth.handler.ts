b5ccbd860709794e12639a4292dda5a0207abc6c
Frederikke <frny0001@stud.kea.dk>
2026-05-13 11:36:29 +0200

    lint(api): linting errors for auth(#101)

--- apps/backend/src/api/auth/auth.handler.ts
+++ apps/backend/src/api/auth/auth.handler.ts
@@ -34,8 +34,8 @@ const splitName = (name: string) => {
 export const signupUser = async (c: Context) => {
   const body = await c.req.json<SignupRequest>();
 
-  const email = body.email?.trim() ?? '';
-  const password = body.password?.trim() ?? '';
+  const email: string = body.email?.trim() ?? '';
+  const password: string = body.password?.trim() ?? '';
 
   if (!email || !password || (!body.name && !body.first_name)) {
     return c.json(
@@ -44,7 +44,7 @@ export const signupUser = async (c: Context) => {
     );
   }
 
-  const names = body.name
+  const names: { first_name: string; last_name: string } = body.name
     ? splitName(body.name)
     : {
         first_name: body.first_name?.trim() ?? '',
