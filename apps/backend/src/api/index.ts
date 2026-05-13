37acdefa2857111fc9952016d0f8905c35d57596
Lydur Hafsteinsson <lydur.hafsteinsson@gmail.com>
2026-05-13 18:10:05 +0200

    feat(api-services): add services enpoints (#135)

--- apps/backend/src/api/index.ts
+++ apps/backend/src/api/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import { health } from './health';
 import { usersRoutes } from './users/users.router';
+import { servicesRoutes } from './services/services.router';
 
 const api = new Hono();
 
 api.route('/health', health);
 api.route('/users', usersRoutes);
+api.route('/services', servicesRoutes);
 
 export { api };
