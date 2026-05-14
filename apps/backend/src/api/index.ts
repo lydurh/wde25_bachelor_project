4286f61b6540b688fa310f54f962ce0099b92009
anton0998 <anton.heiden@hotmail.com>
2026-05-14 18:17:39 +0200

    refactor(api): rename routers for appointments and availability for consistency(#176)

--- apps/backend/src/api/index.ts
+++ apps/backend/src/api/index.ts
@@ -3,9 +3,9 @@ import { health } from './health';
 import { usersRoutes } from './users/users.router';
 import { servicesRoutes } from './services/services.router';
 import { authRoutes } from './auth/auth.router';
-import { appointmentsRouter } from './appointments/appointments.router';
+import { appointmentsRoutes } from './appointments/appointments.router';
 import { locationsRoutes } from './locations/locations.router';
-import { availabilityRouter } from './availability/availability.router';
+import { availabilityRoutes } from './availability/availability.router';
 
 const api = new Hono();
 
@@ -13,8 +13,8 @@ api.route('/health', health);
 api.route('/users', usersRoutes);
 api.route('/services', servicesRoutes);
 api.route('/auth', authRoutes);
-api.route('/appointments', appointmentsRouter);
+api.route('/appointments', appointmentsRoutes);
 api.route('/locations', locationsRoutes);
-api.route('/availability', availabilityRouter);
+api.route('/availability', availabilityRoutes);
 
 export { api };
