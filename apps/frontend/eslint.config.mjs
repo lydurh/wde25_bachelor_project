125c07dfcdab1f9cf2d6031342f5f09942eb4d11
Lydur Hafsteinsson <lydur.hafsteinsson@gmail.com>
2026-05-15 12:59:41 +0200

    feat(frontend): set up global css propreties and import shadcn component library (#186)

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -3,6 +3,9 @@ import baseConfig from '@repo/config/eslint/base.mjs';
 export default [
   ...baseConfig,
   {
+    ignores: ['src/components/ui/**'],
+  },
+  {
     files: ['vite.config.ts'],
     rules: {
       'no-restricted-syntax': 'off',
